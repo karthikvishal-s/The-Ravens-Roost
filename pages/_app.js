54b25708576395c89ce6f5721b0828b9ac76f81b
Karthik Vishal <karthikvishal1506@gmail.com>
2025-06-09 13:28:57 +0530

    favicon check-1

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,22 @@
+// pages/_app.js
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
-import TimeAgo from 'javascript-time-ago'
+import Head from "next/head";
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en';
 
-import en from 'javascript-time-ago/locale/en'
+TimeAgo.addDefaultLocale(en);
 
-
-TimeAgo.addDefaultLocale(en)
-
-
-export default function App({ Component, pageProps:{session,...pageProps} }) {
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-  <SessionProvider session={session}>
-    <Component {...pageProps} />;
-  </SessionProvider>
-  )
+    <SessionProvider session={session}>
+      <Head>
+        <link rel="icon" href="/raven.png" />
+        <title>The Raven's Roost</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Add other meta tags if you want */}
+      </Head>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,16 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* You can add favicon here too, but no need if added in _app.js */}
+        
+        <meta charSet="UTF-8" />
+        <meta name="description" content="Your app description here" />
+        <meta name="author" content="Karthik Vishal" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
