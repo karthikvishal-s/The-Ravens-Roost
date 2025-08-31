741aa437bc0b28e5566a8e4f364915fc255cff84
Karthik Vishal <karthikvishal1506@gmail.com>
2025-08-31 16:46:33 +0530

    Added optimized login for like button

--- pages/api/like.js
+++ pages/api/like.js
@@ -4,40 +4,30 @@ import { getServerSession } from "next-auth/next";
 import Like from "@/models/Like";
 import Post from "@/models/Post";
 
-
-
-async function updateLikesCount(postId) {
-    const post = await Post.findById(postId);
-    post.likesCount = await Like.countDocuments({ post: postId });
-    //eventough if we increase the value in db , it will automatically count the docs and become normal..
-    await post.save();
-}
-
-
-
 export default async function handle(req, res) {
-    await initMongoose();
-    const session = await getServerSession(req, res, authOptions);
-    const postId = req.body.id;
-    const userID = session?.user?._id;
-  
-    console.log("Session:", session); // 🔍 Debug
-    console.log("User ID:", userID); // 🔍 Debug
-  
-    if (!userID) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
-  
-    const existingLike = await Like.findOne({ author: userID, post: postId });
-  
-    if (existingLike) {
-      await existingLike.deleteOne();
-      await updateLikesCount(postId);
-      res.json({ liked: false });
-    } else {
-      await Like.create({ author: userID, post: postId });
-      await updateLikesCount(postId);
-      res.json({ liked: true });
-    }
+  await initMongoose();
+  const session = await getServerSession(req, res, authOptions);
+  const postId = req.body.id;
+  const userID = session?.user?._id;
+
+  if (!userID) {
+    return res.status(401).json({ error: "Unauthorized" });
   }
-  
+
+  // Check if user already liked
+  const existingLike = await Like.findOne({ author: userID, post: postId });
+
+  if (existingLike) {
+    // Unlike: remove Like doc + decrement count
+    await Like.deleteOne({ _id: existingLike._id });
+    await Post.updateOne({ _id: postId }, { $inc: { likesCount: -1 } });
+
+    return res.json({ liked: false });
+  } else {
+    // Like: create Like doc + increment count
+    await Like.create({ author: userID, post: postId });
+    await Post.updateOne({ _id: postId }, { $inc: { likesCount: 1 } });
+
+    return res.json({ liked: true });
+  }
+}
